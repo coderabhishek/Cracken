848b060ff6828dc26a76f1c6a0316d20531818e7
coderabhishek <abhishekkmr18@gmail.com>
2017-04-21 18:01:19 +0530

    No idea where we are

--- util.cpp
+++ util.cpp
@@ -31,6 +31,34 @@ map<string, string> struct_sub_type;
 
 string start_label = "L1";
 
+class types_c{
+	public:
+	map<string, int> types_l;
+	bool end(){
+		return false;
+	}
+	types_c(){
+		types_l["int"] = 4;
+		types_l["char"] = 1;
+		types_l["float"] = 4;
+	}
+	bool find(string s){
+		return (types_l.find(s) != types_l.end());
+	}
+	int get(string t){
+		if(t.size()>2 and t[0]=='p' and t[1]=='_')
+			return 4;
+		else return types_l[t];
+	}
+	void set(string t, int s){
+		types_l[t] = s;
+	}
+};
+
+types_c types = types_c();;
+
+
+
 bool coercible(string t1, string t2){
 	if(t1 == t2)
 		return true;
@@ -87,7 +115,6 @@ code_segment *create(){
 
 stack<string> scope;
 
-map<string, int> types;
 int global_addr = 9000;
 
 int get_addr(string t){
@@ -95,7 +122,7 @@ int get_addr(string t){
 	assert(types.find(t) != types.end());
 
 	auto temp = global_addr;
-	global_addr += types[t];
+	global_addr += types.get(t);
 	return temp;	
 }
 
@@ -113,6 +140,7 @@ class sym_table_cl{
 		int most_recent_scope(string name){
 			if(sym_tab[name].empty()){
 				yyerror("Unknown variable or expression  " + name);
+				return 0;
 			}
 			return sym_tab[name].top().first;
 		}
@@ -128,13 +156,12 @@ class sym_table_cl{
 				return;
 			}	
 			sym_tab[name].push(make_pair(cur_scope, type));
-			addr_table[name + "_" + to_string(cur_scope)] = global_addr;
-			global_addr += types[type];
+			addr_table[name + "_" + to_string(cur_scope)] = types.get(type);
+			global_addr += types.get(type);
 		}	
 
 		string get(string name){
 			if(!sym_tab[name].empty()){
-				assert(!sym_tab[name].empty());
 				return sym_tab[name].top().second;
 			}
 			yyerror("No variable named " + name);
@@ -149,9 +176,19 @@ class sym_table_cl{
 			}
 			cur_scope--;
 		}
-
+		void render();
 };
 
+void sym_table_cl::render(){
+	string ident = "          ";
+	for(auto el: sym_tab){
+		if(el.second.empty())
+			continue;
+		cout<<el.first<<":\n";
+		cout<<ident<<el.second.top().first<<"  "<<el.second.top().second<<"\n";
+		cout<<"\n\n";
+	}
+}
 sym_table_cl sym_table = sym_table_cl::get_singleton(); 
 
 
@@ -181,7 +218,53 @@ string get_complex_type(string basic_type){
 
 
 string find_type(string s){
-	cout<<"FIND TYPE  ->  "<<s<<endl;
+	if(s.find('[') != string::npos){
+		string name;
+		int i=0;
+		while(s[i]!='[')
+			name.push_back(s[i++]);
+		i++;
+		auto t = sym_table.get(name);
+		string res;
+		int j=0;
+		while(t[j]!='_')
+			res.push_back(t[j++]);
+		j++;
+		vector<int> t_v, s_v;
+		while(i<s.size()){
+			string t;
+			while(i<s.size() and isdigit(s[i])){
+				t.push_back(s[i]);
+				i++;
+			}
+			s_v.push_back(stoi(t));
+		i++;
+		i++;	
+		}
+		while(j<t.size()){
+			string te;
+			while(j<t.size() and isdigit(t[j])){
+				te.push_back(t[j]);
+				j++;
+			}
+			t_v.push_back(stoi(te));
+		j++;	
+		}
+		i = 0;
+		for(i=0;i<t_v.size() and i<s_v.size();++i)
+			if(t_v[i]<=s_v[i])
+				yyerror("Dimensions mismatched!!");
+		if(i==s_v.size() and i<t_v.size()){
+			for(;i<t_v.size();++i)
+				res += "_" + to_string(t_v[i]);
+		}
+		else if(i==t_v.size() and i<s_v.size()){
+			yyerror("Dimensions mismatched!!");
+			return "";
+		}
+		return res;	
+
+	}
 	if(s[0] == '*'){
 		s.erase(s.begin());
 		auto res = find_type(s);
@@ -249,21 +332,25 @@ struct param_list{
 };
 vector<func> funcs;
 
-void types_init(){
-	types["int"] = 4;
-	types["char"] = 1;
-	types["float"] = 4;
-}
-
 int size_of_param_frame(vector<string> &v){
 	auto res = 0;
 	for(auto el: v){
-		res += types[el];
+		res += types.get(el);
 	}
 	return res;
 }
 
 string scoped_name(string s){
+	if(s.find('[')!=string::npos){
+		string name;
+		int i=0;
+		while(s[i]!='[')
+			name.push_back(s[i++]);
+		name.append("_" + to_string(sym_table.most_recent_scope(name)));
+		for(;i<s.size();++i)
+			name.push_back(s[i]);
+		return name;
+	}
 	if(s[0] == '&'){
 		s.erase(s.begin());
 		return "&" + scoped_name(s);
@@ -271,6 +358,16 @@ string scoped_name(string s){
 	if(s[0] == '*'){
 		s.erase(s.begin());
 		return "*" + scoped_name(s);
+	}
+	if(s.find(".")!= string::npos){
+		auto temp = s;
+		while(s[s.size()-1] != '.')
+			s.pop_back();
+		s.pop_back();
+		auto res = scoped_name(s);
+		for(int i=temp.find(".");i<temp.size();++i)
+			res.push_back(temp[i]);
+		return res;
 	}
 	return s + "_" + to_string(sym_table.most_recent_scope(s));
 }
